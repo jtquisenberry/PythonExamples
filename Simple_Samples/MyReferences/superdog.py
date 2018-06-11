a0bb707dda80b9b4f1e9ce9d166de9d7e44a7556
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2018-06-10 22:00:13 -0700

    References

---
+++ Simple_Samples/MyReferences/superdog.py
@@ -0,0 +1,6 @@
+from ..reference2 import i_like_dogs
+
+i_like_dogs()
+#ValueError: attempted relative import beyond top-level package
+
+# https://stackoverflow.com/questions/30669474/beyond-top-level-package-error-in-relative-import
---
+++ Simple_Samples/reference2.py
@@ -0,0 +1,4 @@
+def i_like_dogs():
+    print('I like dogs')
+
+print('I like dogs.')
