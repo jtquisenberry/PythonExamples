163e84a16a8f775a01895ae0919ee3878d41df72
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2019-08-16 14:35:08 -0700

    Added data

---
+++ Pandas_Examples/dict_to_dataframe.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
+df = pd.DataFrame.from_dict(data)
--- Pandas_Examples/write_to_excel.py
+++ Pandas_Examples/write_to_excel.py
@@ -2,26 +2,23 @@ import pandas as pd
 import openpyxl
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 
-
-
-df_db = pd.DataFrame(columns=['db_id', 'db_guid', 'db_tag', 'db_start_position', 'db_word_pos', 'db_num_pos', 'db_pattern', 'db_match_text', 'db_score'])
-
-# Add Data
-# TODO: Add Data
+data = {'col_1': [3, 2, 1, 0], 'col_2': ['=a', 'b', 'c', 'd']}
+df = pd.DataFrame.from_dict(data)
 
 # Remove leading "=", or Excel will interpret the value as an invalid formula.
-df_db['db_match_text'] = df_db['db_match_text'].apply(lambda x: ';' + x[1:] if x[0] == '=' else x)
+df['col_2'] = df['col_2'].apply(lambda x: ';' + x[1:] if x[0] == '=' else x)
 
 # Remove illegal characters using a Regex.
-df_db['db_match_text'] = df_db['db_match_text'].replace(ILLEGAL_CHARACTERS_RE, ' ', regex=True)
+df['col_2'] = df['col_2'].replace(ILLEGAL_CHARACTERS_RE, ' ', regex=True)
 
 # Optional: Convert Unicode to escapes.
 # This method operates on all columns.
+'''
 df_db = df_db.applymap(lambda x: x.encode('unicode_escape').
                  decode('utf-8') if isinstance(x, str) else x)
-
+'''
 
 # Use engine openpyxl becaue it is better at throwing errors at runtime.
 # Engine xlsxwriter may succeed at writing to Excel but produce a 
 # corrupted Excel.
-df_db.to_excel(r'E:\Development\Python\pii_elastic\eeee.xlsx', engine='openpyxl')
+df.to_excel(r'C:\temp\aaa.xlsx', engine='openpyxl')
