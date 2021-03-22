76e65ef0875eac760619c7e31eb56ac85efe2dd0
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2021-03-21 22:01:36 -0700

    numpy_examples

---
+++ numpy_examples/matrix_multiplication.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+print("Multiplication with For Loop")
+print()
+
+X = \
+    [
+        [3, 2, 8],
+        [3, 3, 4],
+        [7, 2, 5]
+    ]
+
+Y = \
+    [
+        [2, 3],
+        [4, 2],
+        [5, 5]
+    ]
+
+# out = [([0] * len(Y[0]))] * len(X)
+out = [[0, 0],[0, 0],[0, 0]]
+# out = []
+out = [[0] * 2 for i in range(3)]
+
+print('X')
+print(X)
+print('Y')
+print(Y)
+print('Product')
+
+
+for i in range(len(X)):
+    for j in range(len(Y[0])):
+        for k in range(len(Y)):
+            jjj = X[i][k] * Y[k][j]
+            out[i][j] += X[i][k] * Y[k][j]
+
+
+print(np.array(out))
+'''
+print(out)
+'''
+print(np.dot(X,Y))
+
+'''
+
+
+
+
+
+
+
+
+
+
+X_arr = np.array(X)
+Y_arr = np.array(Y)
+print(X)
+print(Y)
+
+z = np.dot(X_arr, Y_arr)
+print(z)
+
+'''
