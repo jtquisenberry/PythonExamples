132aaa7729f16bcb98e23aabfadb224e7f7a6d3f
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2021-10-27 21:48:25 -0700

    reverse_words

--- Interview_Cake/arrays/reverse_words_in_list_in_place_working.py
+++ Interview_Cake/arrays/reverse_words_in_list_in_place_working.py
@@ -54,77 +54,87 @@ import unittest
 def reverse_words(message):
 
     # Decode the message by reversing the words
-
-
-    pass
-
-
-def reverse_characters(s):
-
+    reverse_characters(message, 0, len(message) - 1)
     start_index = 0
-    end_index = len(s) - 1
+    end_index = 0
+    for i in range(len(message)):
+        if message[i] == ' ':
+            end_index = i - 1
+            reverse_characters(message, start_index, end_index)
+            # print(message[start_index: end_index + 1])
+            start_index = i + 1
+            a = 1
+        if i == len(message) - 1:
+            end_index = i
+            reverse_characters(message, start_index, end_index)
+            # print(message[start_index: end_index + 1])
+            a = 1
+
+
+def reverse_characters(substring, start_index, end_index):
+
     while start_index < end_index:
-        s[start_index], s[end_index] = s[end_index], s[start_index]
+        substring[start_index], substring[end_index] = substring[end_index], substring[start_index]
         start_index += 1
         end_index -= 1
     return
 
-a = ['c', 'o', 'w', 's']
-print(reverse_characters(a))
-a = 1
+# a = ['c', 'o', 'w', 's']
+# print(reverse_characters(a))
+# a = 1
 
 
+message = list('one another get')
+reverse_words(message)
+expected = list('get another one')
+actual = message
+print(expected == actual)
 
 
+print('test_one_word')
+message = list('vault')
+reverse_words(message)
+expected = list('vault')
+actual = message
+print(actual == expected)
 
 
+print('test_two_words')
+message = list('thief cake')
+reverse_words(message)
+expected = list('cake thief')
+actual = message
+print(actual == expected)
 
 
+print('test_three_words')
+message = list('one another get')
+reverse_words(message)
+expected = list('get another one')
+actual = message
+print(actual == expected)
 
 
+print('test_multiple_words_same_length')
+message = list('rat the ate cat the')
+reverse_words(message)
+expected = list('the cat ate the rat')
+actual = message
+print(actual == expected)
 
 
+print('test_multiple_words_different_lengths')
+message = list('yummy is cake bundt chocolate')
+reverse_words(message)
+expected = list('chocolate bundt cake is yummy')
+actual = message
+print(actual == expected)
 
 
-# Tests
+print('test_empty_string')
+message = list('')
+reverse_words(message)
+expected = list('')
+actual = message
+print(actual == expected)
 
-class Test(unittest.TestCase):
-
-    def test_one_word(self):
-        message = list('vault')
-        reverse_words(message)
-        expected = list('vault')
-        self.assertEqual(message, expected)
-
-    def test_two_words(self):
-        message = list('thief cake')
-        reverse_words(message)
-        expected = list('cake thief')
-        self.assertEqual(message, expected)
-
-    def test_three_words(self):
-        message = list('one another get')
-        reverse_words(message)
-        expected = list('get another one')
-        self.assertEqual(message, expected)
-
-    def test_multiple_words_same_length(self):
-        message = list('rat the ate cat the')
-        reverse_words(message)
-        expected = list('the cat ate the rat')
-        self.assertEqual(message, expected)
-
-    def test_multiple_words_different_lengths(self):
-        message = list('yummy is cake bundt chocolate')
-        reverse_words(message)
-        expected = list('chocolate bundt cake is yummy')
-        self.assertEqual(message, expected)
-
-    def test_empty_string(self):
-        message = list('')
-        reverse_words(message)
-        expected = list('')
-        self.assertEqual(message, expected)
-
-
-unittest.main(verbosity=2)
