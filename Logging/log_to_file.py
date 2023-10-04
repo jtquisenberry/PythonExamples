14b9d947882f67e19102e79ed33c9638e1451b6b
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-10-04 17:33:25 -0500

    Create log_to_file.py

---
+++ Logging/log_to_file.py
@@ -0,0 +1,9 @@
+import logging
+log_name = './my_log.log'
+logging.basicConfig(filename=log_name,
+                    filemode='a',
+                    format='%(asctime)s, %(name)s %(levelname)s %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger("main")
+logger.info("Test")
+print("DONE")
