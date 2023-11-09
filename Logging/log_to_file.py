1a888c86ac17ac71a14bc0563d443d55df7acb1b
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-11-08 20:14:19 -0600

    Update log_to_file.py

--- Logging/log_to_file.py
+++ Logging/log_to_file.py
@@ -1,9 +1,11 @@
 import logging
+import sys
 log_name = './my_log.log'
 logging.basicConfig(filename=log_name,
                     filemode='a',
                     format='%(asctime)s, %(name)s %(levelname)s %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger("main")
+logger.addHandler(logging.StreamHandler(sys.stdout))
 logger.info("Test")
 print("DONE")
