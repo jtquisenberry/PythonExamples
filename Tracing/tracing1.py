543f5d6b9b47e6477a4a2b3efa126ed63da60e35
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2024-11-19 20:47:22 -0600

    Coverage

--- Tracing/tracing1.py
+++ Tracing/tracing1.py
@@ -1,14 +1,55 @@
 from multiprocessing import Process
 from tracing3 import target3
+from threading import Thread
+import time
 
 
-def target_func():
-    print("target start")
+class MyThread(Thread):
+    def __init__(self):
+        super().__init__()
+        self.a = 1
+        self.b = 2
+
+    def run(self):
+        x = 0
+        while x <= 500:
+            if x % 100 == 0:
+                print(x)
+            x += 1
+        return True
+
+
+class MyThread2(Thread):
+    def __init__(self):
+        super().__init__()
+        self.a = 1
+        self.b = 2
+
+    def run(self):
+        x = 1000
+        while x <= 1500:
+            if x % 100 == 0:
+                print(x)
+            x += 1
+        return True
+
+
+
+
+def target3():
+    print("target3")
+    t = MyThread2()
+    t.start()
+    time.sleep(1)
+    print("target3")
+
 
 def func2():
     print("func2")
     p = Process(target=target3)
     p.start()
+    t = MyThread()
+    t.start()
 
 def main():
     print("main")
--- Tracing/tracing3.py
+++ Tracing/tracing3.py
@@ -2,5 +2,7 @@ import time
 
 def target3():
     print("target3")
+    t = MyThread()
+    t.start()
     time.sleep(1)
     print("target3")
