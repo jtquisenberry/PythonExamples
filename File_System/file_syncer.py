52a0ef03c3b2364f3d0d87dbf39983a6d06e5d80
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2025-03-05 19:22:05 -0600

    FileSyncer

--- File_System/file_syncer.py
+++ File_System/file_syncer.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import random
+import timeit
+
 
 class FileSyncer:
     def __init__(self, local_path='', remote_path=''):
@@ -65,17 +67,37 @@ class FileSyncer:
             if left_file_size != right_file_size:
                 return False
 
+            with open(left_fq_filename, 'rb') as left_file, open(right_fq_filename, 'rb') as right_file:
+                a = left_file.read()
+                b = right_file.read()
+                # print(a == b)
+                # c = 1
+                c = a == b
 
-            for i in range(left_file_size):
+    def evaluate_file_matches2(self):
+        for partial_filename in self.in_both:
+            left_fq_filename = os.path.join(self.local_path, partial_filename)
+            right_fq_filename = os.path.join(self.remote_path, partial_filename)
+            left_file_size = os.path.getsize(left_fq_filename)
+            right_file_size = os.path.getsize(right_fq_filename)
+            if left_file_size != right_file_size:
+                return False
+            if not os.path.isfile(left_fq_filename):
+                return True
 
-
-
-            with open(left_fq_filename, 'rb') as left_file:
-                pass
-
-            with open(right_fq_filename, 'rb') as right_file:
-                pass
-
+            left_bytes = list()
+            right_bytes = list()
+            with open(left_fq_filename, 'rb') as left_file, open(right_fq_filename, 'rb') as right_file:
+                for i in range(100):
+                    position = random.randint(0, left_file_size)
+                    left_file.seek(position)
+                    left_byte = left_file.read(1)
+                    left_bytes.append(left_byte)
+                    right_file.seek(position)
+                    right_byte = right_file.read(1)
+                    right_bytes.append(right_byte)
+                # print(left_bytes == right_bytes)
+                xxx = left_bytes == right_bytes
 
 
 
@@ -83,13 +105,14 @@ class FileSyncer:
 fs = FileSyncer(local_path='D:\\Development\\temp\\',
                 remote_path=r'D:\Development\temp2')
 
-fs.evaluate_path_matches()
+time_taken_function = timeit.timeit(fs.evaluate_path_matches, number=10000)
+print(f"Time taken for function: {time_taken_function} seconds")
 
-x = fs.get_files2(fs.local_path)
+time_taken_function = timeit.timeit(fs.evaluate_file_matches2, number=10000)
+print(f"Time taken for function: {time_taken_function} seconds")
 
-z = 1
+# fs.evaluate_path_matches()
+# fs.evaluate_file_matches2()
 
 
-    # Obține lista de fișiere pentru fiecare folder
-    # files_folder1 = get_files(folder1)
-    # files_folder2 = get_files(folder2)
+
