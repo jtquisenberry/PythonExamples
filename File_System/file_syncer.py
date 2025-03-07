2dd43522fbb532c44b342b15d41a80dec7193c15
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2025-03-06 20:28:42 -0600

    FileSyncer

--- File_System/file_syncer.py
+++ File_System/file_syncer.py
@@ -1,6 +1,8 @@
 import os
 import glob
 import random
+import hashlib
+import filecmp
 import timeit
 
 
@@ -58,7 +60,7 @@ class FileSyncer:
         self.right_only = right_files.difference(left_files)
         self.in_both = left_files.intersection(right_files)
 
-    def evaluate_file_matches(self):
+    def evaluate_file_matches_full(self):
         for partial_filename in self.in_both:
             left_fq_filename = os.path.join(self.local_path, partial_filename)
             right_fq_filename = os.path.join(self.remote_path, partial_filename)
@@ -66,15 +68,16 @@ class FileSyncer:
             right_file_size = os.path.getsize(right_fq_filename)
             if left_file_size != right_file_size:
                 return False
+            if not os.path.isfile(left_fq_filename):
+                return True
 
             with open(left_fq_filename, 'rb') as left_file, open(right_fq_filename, 'rb') as right_file:
                 a = left_file.read()
                 b = right_file.read()
                 # print(a == b)
-                # c = 1
                 c = a == b
 
-    def evaluate_file_matches2(self):
+    def evaluate_file_matches_random_100(self):
         for partial_filename in self.in_both:
             left_fq_filename = os.path.join(self.local_path, partial_filename)
             right_fq_filename = os.path.join(self.remote_path, partial_filename)
@@ -96,19 +99,87 @@ class FileSyncer:
                     right_file.seek(position)
                     right_byte = right_file.read(1)
                     right_bytes.append(right_byte)
-                # print(left_bytes == right_bytes)
-                xxx = left_bytes == right_bytes
+                c = left_bytes == right_bytes
+
+    def evaluate_file_matches_random_100_sorted(self):
+        for partial_filename in self.in_both:
+            left_fq_filename = os.path.join(self.local_path, partial_filename)
+            right_fq_filename = os.path.join(self.remote_path, partial_filename)
+            left_file_size = os.path.getsize(left_fq_filename)
+            right_file_size = os.path.getsize(right_fq_filename)
+            if left_file_size != right_file_size:
+                return False
+            if not os.path.isfile(left_fq_filename):
+                return True
+
+            left_bytes = list()
+            right_bytes = list()
+            with open(left_fq_filename, 'rb') as left_file, open(right_fq_filename, 'rb') as right_file:
+                positions = list()
+                for i in range(100):
+                    position = random.randint(0, left_file_size)
+                    positions.append(position)
+                positions.sort()
+                for position in positions:
+                    left_file.seek(position)
+                    left_byte = left_file.read(1)
+                    left_bytes.append(left_byte)
+                    right_file.seek(position)
+                    right_byte = right_file.read(1)
+                    right_bytes.append(right_byte)
+                c = left_bytes == right_bytes
+
+    def evaluate_file_matches_filecmp(self):
+        for partial_filename in self.in_both:
+            left_fq_filename = os.path.join(self.local_path, partial_filename)
+            right_fq_filename = os.path.join(self.remote_path, partial_filename)
+            left_file_size = os.path.getsize(left_fq_filename)
+            right_file_size = os.path.getsize(right_fq_filename)
+            if left_file_size != right_file_size:
+                return False
+            if not os.path.isfile(left_fq_filename):
+                return True
+
+            c = filecmp.cmp(left_fq_filename, right_fq_filename, shallow=False)
 
 
+    def evaluate_file_matches_md5(self):
+        for partial_filename in self.in_both:
+            left_fq_filename = os.path.join(self.local_path, partial_filename)
+            right_fq_filename = os.path.join(self.remote_path, partial_filename)
+            left_file_size = os.path.getsize(left_fq_filename)
+            right_file_size = os.path.getsize(right_fq_filename)
+            if left_file_size != right_file_size:
+                return False
+            if not os.path.isfile(left_fq_filename):
+                return True
+
+            with open(left_fq_filename, 'rb') as left_file, open(right_fq_filename, 'rb') as right_file:
+                left_binary = left_file.read()
+                right_binary = right_file.read()
+                left_md5 = hashlib.md5(left_binary).hexdigest()
+                right_md5 = hashlib.md5(right_binary).hexdigest()
+                c = left_md5 == right_md5
 
 
 fs = FileSyncer(local_path='D:\\Development\\temp\\',
                 remote_path=r'D:\Development\temp2')
 
-time_taken_function = timeit.timeit(fs.evaluate_path_matches, number=10000)
+fs.evaluate_path_matches()
+
+time_taken_function = timeit.timeit(fs.evaluate_file_matches_full, number=10000)
 print(f"Time taken for function: {time_taken_function} seconds")
 
-time_taken_function = timeit.timeit(fs.evaluate_file_matches2, number=10000)
+time_taken_function = timeit.timeit(fs.evaluate_file_matches_random_100, number=10000)
+print(f"Time taken for function: {time_taken_function} seconds")
+
+time_taken_function = timeit.timeit(fs.evaluate_file_matches_random_100_sorted, number=10000)
+print(f"Time taken for function: {time_taken_function} seconds")
+
+time_taken_function = timeit.timeit(fs.evaluate_file_matches_filecmp, number=10000)
+print(f"Time taken for function: {time_taken_function} seconds")
+
+time_taken_function = timeit.timeit(fs.evaluate_file_matches_md5, number=10000)
 print(f"Time taken for function: {time_taken_function} seconds")
 
 # fs.evaluate_path_matches()
